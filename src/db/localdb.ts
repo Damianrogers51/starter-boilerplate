b4afbf356fcb9d1d25f78a1f64fa9d45604e7a64
Damian rogers <damianrogers51@gmail.com>
2024-07-27 15:22:39 -0700

    add local database store

---
+++ src/db/localdb.ts
@@ -0,0 +1,134 @@
+import { SelectBrand } from "./schema";
+
+export const brands: Array<SelectBrand> = [
+  {
+    id: 0,
+    name: "Discont'd",
+    value: "discontd",
+    instagram: "discontd",
+    website: "https://discontd.com/collections/shop",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_discontd.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_discontd_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_discontd_2.png"
+    ]
+  },
+  {
+    id: 1,
+    name: "Entire Studios",
+    value: "entirestudios",
+    instagram: "entire_studios",
+    website: "https://www.entirestudios.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_entirestudios.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_2.png"
+    ]
+  },
+  {
+    id: 2,
+    name: "Vuja De",
+    value: "vujade",
+    instagram: "vujadestudio",
+    website: "https://www.vujade-studio.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_vujade.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_vujade_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_vujade_2.png"
+    ]
+  },
+  {
+    id: 3,
+    name: "about:blank",
+    value: "abountblank",
+    instagram: "about___blank",
+    website: "https://about---blank.com/en-us",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_aboutblank.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_2.png"
+    ]
+  },
+  {
+    id: 4,
+    name: "California Arts",
+    value: "californiaarts",
+    instagram: "california.arts",
+    website: "https://california-arts.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_californiaarts.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_californiaarts_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_californiaarts_2.png"
+    ]
+  },
+  {
+    id: 5,
+    name: "O. Files",
+    value: "ofiles",
+    instagram: "ofiless",
+    website: "https://o-files.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_discontd.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ofiles_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ofiles_2.png"
+    ]
+  },
+  {
+    id: 6,
+    name: "Cmmawear",
+    value: "cmmawear",
+    instagram: "cmmawear",
+    website: "https://cmmawear.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_cmmawear.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_cmmawear_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_cmmawear_2.png"
+    ]
+  },
+  {
+    id: 7,
+    name: "Pearled Ivory",
+    value: "pearledivory",
+    instagram: "pearledivory",
+    website: "https://www.pearledivory.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_pearledivory.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_pearledivory_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_pearledivory_2.png"
+    ]
+  },
+  {
+    id: 8,
+    name: "Art-E-Ficial",
+    value: "arteficial",
+    instagram: "arteficialworld",
+    website: "https://art-e-ficial.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_arteficial.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_2.png"
+    ]
+  },
+  {
+    id: 9,
+    name: "Jooniverse Lab",
+    value: "jooniverselab",
+    instagram: "jooniverselab",
+    website: "https://www.jooniverselab.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_jooniverselab.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_jooniverselab_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_jooniverselab_2.png"
+    ]
+  },
+]
