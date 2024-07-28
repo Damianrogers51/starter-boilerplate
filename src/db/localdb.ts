94a68dc685e326d37ac3f7212ef41dcebd616c7f
Damian rogers <damianrogers51@gmail.com>
2024-07-27 23:27:26 -0700

    add new rows to local database

--- src/db/localdb.ts
+++ src/db/localdb.ts
@@ -6,6 +6,7 @@ export const brands: Array<SelectBrand> = [
     name: "Discont'd",
     value: "discontd",
     instagram: "discontd",
+    followers: 27,
     website: "https://discontd.com/collections/shop",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_discontd.jpg",
@@ -14,11 +15,13 @@ export const brands: Array<SelectBrand> = [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_discontd_2.png"
     ]
   },
+  /*
   {
     id: 1,
     name: "Entire Studios",
     value: "entirestudios",
     instagram: "entire_studios",
+    followers: 235,
     website: "https://www.entirestudios.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_entirestudios.jpg",
@@ -27,11 +30,13 @@ export const brands: Array<SelectBrand> = [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_2.png"
     ]
   },
+  */
   {
     id: 2,
     name: "Vuja De",
     value: "vujade",
     instagram: "vujadestudio",
+    followers: 97,
     website: "https://www.vujade-studio.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_vujade.jpg",
@@ -40,11 +45,13 @@ export const brands: Array<SelectBrand> = [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_vujade_2.png"
     ]
   },
+  /*
   {
     id: 3,
     name: "about:blank",
     value: "abountblank",
     instagram: "about___blank",
+    followers: 331,
     website: "https://about---blank.com/en-us",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_aboutblank.jpg",
@@ -53,11 +60,13 @@ export const brands: Array<SelectBrand> = [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_2.png"
     ]
   },
+  */
   {
     id: 4,
     name: "California Arts",
     value: "californiaarts",
     instagram: "california.arts",
+    followers: 54,
     website: "https://california-arts.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_californiaarts.jpg",
@@ -71,6 +80,7 @@ export const brands: Array<SelectBrand> = [
     name: "O. Files",
     value: "ofiles",
     instagram: "ofiless",
+    followers: 25,
     website: "https://o-files.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_discontd.jpg",
@@ -84,6 +94,7 @@ export const brands: Array<SelectBrand> = [
     name: "Cmmawear",
     value: "cmmawear",
     instagram: "cmmawear",
+    followers: 44,
     website: "https://cmmawear.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_cmmawear.jpg",
@@ -97,6 +108,7 @@ export const brands: Array<SelectBrand> = [
     name: "Pearled Ivory",
     value: "pearledivory",
     instagram: "pearledivory",
+    followers: 30,
     website: "https://www.pearledivory.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_pearledivory.jpg",
@@ -105,11 +117,13 @@ export const brands: Array<SelectBrand> = [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_pearledivory_2.png"
     ]
   },
+  /*
   {
     id: 8,
     name: "Art-E-Ficial",
     value: "arteficial",
     instagram: "arteficialworld",
+    followers: 8,
     website: "https://art-e-ficial.com/",
     thumbnail:
       "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_arteficial.jpg",
@@ -118,17 +132,133 @@ export const brands: Array<SelectBrand> = [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_2.png"
     ]
   },
+  */
   {
     id: 9,
+    name: "K-two Studios",
+    value: "ktwostudios",
+    instagram: "ktwo.studios",
+    followers: 54,
+    website: "https://ktwostudios.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_ktwostudios.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ktwostudios_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ktwostudios_2.png"
+    ]
+  },
+  {
+    id: 10,
     name: "Jooniverse Lab",
     value: "jooniverselab",
     instagram: "jooniverselab",
+    followers: 45,
     website: "https://www.jooniverselab.com/",
     thumbnail:
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_jooniverselab.jpg",
+      "",
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_jooniverselab_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_jooniverselab_2.png"
     ]
   },
+  {
+    id: 11,
+    name: "Somar",
+    value: "somar",
+    instagram: "somar.us",
+    followers: 37,
+    website: "https://somar.us/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_somar.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_somar_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_somar_2.png"
+    ]
+  },
+  {
+    id: 12,
+    name: "Bare Knuckles",
+    value: "bareknuckles",
+    instagram: "bareknucklesco",
+    followers: 61,
+    website: "https://bareknuckles.co/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_bareknuckles.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_bareknuckles_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_bareknuckles_2.png"
+    ]
+  },
+  {
+    id: 13,
+    name: "Circle Author",
+    value: "circleauthor",
+    instagram: "circleauthor",
+    followers: 26,
+    website: "https://circleauthor.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_circleauthor.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_circleauthor_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_circleauthor_2.png"
+    ]
+  },
+  {
+    id: 14,
+    name: "Lac Demure",
+    value: "lacdemure",
+    instagram: "lacdemure",
+    followers: 30,
+    website: "https://lacdemure.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_lacdemure.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_lacdemure_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_lacdemure_2.png"
+    ]
+  },
+  /*
+  {
+    id: 1,
+    name: "Entire Studios",
+    value: "entirestudios",
+    instagram: "entire_studios",
+    followers: 235,
+    website: "https://www.entirestudios.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_entirestudios.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_2.png"
+    ]
+  },
+  {
+    id: 3,
+    name: "about:blank",
+    value: "abountblank",
+    instagram: "about___blank",
+    followers: 331,
+    website: "https://about---blank.com/en-us",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_aboutblank.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_2.png"
+    ]
+  },
+  {
+    id: 8,
+    name: "Art-E-Ficial",
+    value: "arteficial",
+    instagram: "arteficialworld",
+    followers: 8,
+    website: "https://art-e-ficial.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_arteficial.jpg",
+    images: [
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_1.png",
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_2.png"
+    ]
+  },
+  */
 ]
