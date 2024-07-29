e4aa28c842d1c2f1b40e163137a40560efe86e16
Damian rogers <damianrogers51@gmail.com>
2024-07-28 21:20:06 -0700

    add search params filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
 import FilterLink from "@/components/home/FilterLink";
-import { getBrandsAction } from "@/components/home/actions";
-import Image from "next/image";
-import Link from "next/link";
+import Brands from "@/components/home/Brands";
+import Items from "@/components/home/Items";
+import { getBrandsAction, getItemsAction } from "@/components/home/actions";
+import { SelectBrand, SelectItem } from "@/db/schema";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const brands = await getBrandsAction()
+export interface SelectItemWithBrand extends SelectItem {
+  brandData: SelectBrand
+}
+
+export default async function Home({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | undefined }
+}) {
+  let {brands, items} = parseSearchParams()
+
+  function parseSearchParams() {
+    if (!Object.keys(searchParams).length) {
+      return { brands: getBrandsAction(), items: undefined }
+    }
+    else if (searchParams.filter) {
+      if ((searchParams.filter == "baggy_denim") ||
+          (searchParams.filter == "basics") ||
+          (searchParams.filter == "boxy_hoodies")) return { brands: undefined, items: getItemsAction(searchParams.filter) }
+
+      redirect("/")
+    }
+    else {
+      redirect("/")
+    }
+  }
 
   return (
     <div className="xl:36 lg:px-24 md:px-12 sm:px-8 px-4 space-y-8 text-sm py-8">
@@ -17,38 +43,18 @@ export default async function Home() {
 
       <div className="flex justify-center space-x-6">
         <FilterLink href="/"> Brands </FilterLink>
-        <FilterLink href="/"> Baggy Denim </FilterLink>
-        <FilterLink href="/"> Basics </FilterLink>
+        <FilterLink href="/?filter=baggy_denim"> Baggy Denim </FilterLink>
+        <FilterLink href="/?filter=basics"> Basics </FilterLink>
       </div>
 
       <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-x-6 gap-y-8 size-full">
-        {brands.map((brand) => (
-          <div key={brand.id} className="leading-[1.2] space-y-2">
-            <Link href="https://instagram.com/discontd" target="_blank" className="block relative w-full aspect-[4/3] group">
-              <Image src={brand.images[0]} alt={`Cover image for ${brand.name}`} className="object-cover object-center rounded-lg" fill />
-              <Image src={brand.images[1]} alt={`Product image for ${brand.name}`} className="absolute top-0 left-0 object-cover object-center group-hover:opacity-100 opacity-0 transition duration-200 rounded-lg z-10" fill />
-            </Link>
+        {brands && (
+          <Brands brands={brands} />
+        )}
 
-            <div className="flex items-center space-x-2">
-              <div>
-              {brand.thumbnail.length != 0 && (
-                <Image src={brand.thumbnail} alt={`Thumbnail image for ${brand}`} className="rounded-full" width={36} height={36} /> 
-              )}
-              {brand.thumbnail.length == 0 && (
-                <div className="flex items-center justify-center size-[31.2px] backdrop-brightness-105 font-medium text-xs rounded-full select-none">{brand.name[0].toUpperCase()}</div>
-              )}
-              </div>
-
-              <div className="w-full">
-                <div className="w-full flex">
-                  <h3 className="grow font-medium tracking-[-.5px]"> {brand.name} </h3>
-                  <span className="font-light tracking-[-1px] opacity-80"> {brand.followers} K </span>
-                </div>
-                <Link href={`https://instagram.com/${brand.instagram}`} target="_blank" className="font-light tracking-[-.5px] opacity-80"> @{brand.instagram} </Link>
-              </div>
-            </div>
-          </div>
-        ))}
+        {items && (
+          <Items items={items} />
+        )}
       </div>
     </div>
   )
