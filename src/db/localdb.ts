9b19266d37d97d67e7a62d4b99c23af7bf8f6f78
Damian rogers <damianrogers51@gmail.com>
2024-07-28 21:19:50 -0700

    add items local db

--- src/db/localdb.ts
+++ src/db/localdb.ts
@@ -1,4 +1,4 @@
-import { SelectBrand } from "./schema";
+import { SelectBrand, SelectItem } from "./schema";
 
 export const brands: Array<SelectBrand> = [
   {
@@ -13,9 +13,9 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_discontd_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_discontd_2.png"
-    ]
+    ],
+    display: true,
   },
-  /*
   {
     id: 1,
     name: "Entire Studios",
@@ -28,9 +28,9 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_2.png"
-    ]
+    ],
+    display: false
   },
-  */
   {
     id: 2,
     name: "Vuja De",
@@ -43,9 +43,9 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_vujade_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_vujade_2.png"
-    ]
+    ],
+    display: true
   },
-  /*
   {
     id: 3,
     name: "about:blank",
@@ -58,9 +58,9 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_2.png"
-    ]
+    ],
+    display: false
   },
-  */
   {
     id: 4,
     name: "California Arts",
@@ -73,7 +73,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_californiaarts_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_californiaarts_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 5,
@@ -87,7 +88,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ofiles_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ofiles_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 6,
@@ -101,7 +103,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_cmmawear_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_cmmawear_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 7,
@@ -115,9 +118,9 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_pearledivory_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_pearledivory_2.png"
-    ]
+    ],
+    display: true
   },
-  /*
   {
     id: 8,
     name: "Art-E-Ficial",
@@ -130,9 +133,9 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_2.png"
-    ]
+    ],
+    display: false
   },
-  */
   {
     id: 9,
     name: "K-two Studios",
@@ -145,7 +148,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ktwostudios_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_ktwostudios_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 10,
@@ -159,7 +163,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_jooniverselab_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_jooniverselab_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 11,
@@ -173,7 +178,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_somar_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_somar_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 12,
@@ -187,7 +193,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_bareknuckles_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_bareknuckles_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 13,
@@ -201,7 +208,8 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_circleauthor_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_circleauthor_2.png"
-    ]
+    ],
+    display: true
   },
   {
     id: 14,
@@ -215,50 +223,142 @@ export const brands: Array<SelectBrand> = [
     images: [
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_lacdemure_1.png",
       "https://directory-curatd.s3.us-east-2.amazonaws.com/image_lacdemure_2.png"
-    ]
+    ],
+    display: true
   },
-  /*
+  {
+    id: 15,
+    name: "Object 22",
+    value: "object22",
+    instagram: "0bject22",
+    followers: 8,
+    website: "https://www.object-22.com/",
+    thumbnail:
+      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_object22.jpg",
+    images: [],
+    display: false
+  },
+]
+
+export const items: Array<SelectItem> = [
+  {
+    id: 0,
+    brand: 0,
+    name: "Selveged Baggy Denim",
+    price: 203,
+    link: "https://discontd.com/collections/shop/products/selvedge-baggy-denim",
+    images: [
+      "",
+      ""
+    ],
+    tags: ["baggy_denim"]
+  },
   {
     id: 1,
-    name: "Entire Studios",
-    value: "entirestudios",
-    instagram: "entire_studios",
-    followers: 235,
-    website: "https://www.entirestudios.com/",
-    thumbnail:
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_entirestudios.jpg",
+    brand: 4,
+    name: "Laurel Relaxed Wide-Leg Jeans",
+    price: 148,
+    link: "https://california-arts.com/collections/category-nav-denim/products/laurelwidelegjeans_midnight-wash?variant=47880961818904",
     images: [
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_1.png",
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_entirestudios_2.png"
-    ]
+      "",
+      ""
+    ],
+    tags: ["baggy_denim"]
+  },
+  {
+    id: 2,
+    brand: 4,
+    name: "Frank Baby Tee",
+    price: 48,
+    link: "https://california-arts.com/collections/collection-t-shirts-tanks/products/frankbabytee_cloud?variant=47694456914200",
+    images: [
+      "",
+      "",
+    ],
+    tags: ["basics"]
   },
   {
     id: 3,
-    name: "about:blank",
-    value: "abountblank",
-    instagram: "about___blank",
-    followers: 331,
-    website: "https://about---blank.com/en-us",
-    thumbnail:
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_aboutblank.jpg",
+    brand: 7,
+    name: "PI 1211 'Raw Indigo' Denim",
+    price: 210,
+    link: "https://www.pearledivory.com/products/pi-1211-washed-indigo-denim-copy-1",
     images: [
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_1.png",
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_aboutblank_2.png"
-    ]
+      "",
+      "",
+    ],
+    tags: ["baggy_denim"]
+  },
+  {
+    id: 4,
+    brand: 7,
+    name: "Uniform Fitted Tee",
+    price: 70,
+    link: "https://www.pearledivory.com/products/uniform-fitted-tee",
+    images: [
+      "",
+      "",
+    ],
+    tags: ["basics"]
+  },
+  {
+    id: 5,
+    brand: 14,
+    name: "Darius Baggy Jeans",
+    price: 130,
+    link: "https://lacdemure.com/products/darius-baggy-jeans",
+    images: [
+      "",
+      "",
+    ],
+    tags: ["baggy_denim"]
+  },
+  {
+    id: 6,
+    brand: 12,
+    name: "Javier Wide Leg Jeans",
+    price: 248,
+    link: "https://bareknuckles.co/collections/bottoms/products/javier-wide-leg-jeans-black",
+    images: [
+      "",
+      "",
+    ],
+    tags: ["baggy_denim"]
+  },
+  {
+    id: 7,
+    brand: 12,
+    name: "Basics T-Shirt",
+    price: 60,
+    link: "https://bareknuckles.co/collections/tops/products/basics-t-shirt-white",
+    images: [
+      "",
+      "",
+    ],
+    tags: ["basics"]
   },
   {
     id: 8,
-    name: "Art-E-Ficial",
-    value: "arteficial",
-    instagram: "arteficialworld",
-    followers: 8,
-    website: "https://art-e-ficial.com/",
-    thumbnail:
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/thumbnail_arteficial.jpg",
+    brand: 12,
+    name: "Gem Jeans",
+    price: 355,
+    link: "https://www.entirestudios.com/product/gem-jeans-surface-wave",
     images: [
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_1.png",
-      "https://directory-curatd.s3.us-east-2.amazonaws.com/image_arteficial_2.png"
-    ]
+      "",
+      "",
+    ],
+    tags: ["baggy_denim"]
   },
-  */
+  {
+    id: 9,
+    brand: 15,
+    name: "Nocturnal Jeans",
+    price: 365,
+    link: "https://www.object-22.com/nocturnal-jeans/",
+    images: [
+      "",
+      "",
+    ],
+    tags: ["baggy_denim"]
+  },
 ]
